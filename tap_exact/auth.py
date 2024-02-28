32a115c839f57938a0fff204a7ec299f90261673
BernardWez <bernard.wezeman@quantile.nl>
2024-02-28 09:04:46 +0100

    Adjust auth to support Azure blob storage for tokens

--- tap_exact/auth.py
+++ tap_exact/auth.py
@@ -1,14 +1,18 @@
 """Exact Authentication."""
-
 from __future__ import annotations
 
-from singer_sdk.authenticators import OAuthAuthenticator, SingletonMeta
+import backoff
+import os
+import json
+import requests
+import pendulum
+
+from smart_open import open
+from functools import cached_property
+
 from singer_sdk.helpers._util import utc_now
 from singer_sdk.streams import RESTStream
-import backoff
-import requests
-import json
-import boto3
+from singer_sdk.authenticators import OAuthAuthenticator, SingletonMeta
 
 
 class EmptyResponseError(Exception):
@@ -27,47 +31,69 @@ class ExactAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
             stream: A stream for a RESTful endpoint.
         """
         super().__init__(stream)
-        self.s3 = boto3.client("s3")
-        tokens = self.download_tokens_from_s3()
         self._auth_endpoint = "https://start.exactonline.nl/api/oauth2/token"
         self._default_expiration = 600
-        self.refresh_token = tokens["refresh_token"]
+        
+        tokens = self.load_tokens_from_blob()
+
         self.access_token = tokens["access_token"]
+        self.refresh_token = tokens["refresh_token"]
+        self.last_refreshed = pendulum.parse(tokens["last_refreshed"])
+        self.expires_in = 600
+        
+    @cached_property
+    def azure_client(self):
+        from azure.storage.blob import BlobServiceClient
 
-    def download_tokens_from_s3(self) -> dict:
-        return json.loads(
-            self.s3.get_object(
-                Bucket=self._config["tokens_s3_bucket"], Key=self._config["tokens_s3_key"] + "/tokens.json"
-            )
-            .get("Body")
-            .read()
+        return BlobServiceClient.from_connection_string(
+            self.config["azure_connection_string"]
         )
+    
+    def load_tokens_from_blob(self) -> dict:
+        """Loads Exact Online tokens from Azure blob storage using smart_open.
 
-    def put_tokens_in_s3(self, tokens: dict) -> None:
-        self.s3.put_object(
-            Bucket=self._config["tokens_s3_bucket"],
-            Key=self._config["tokens_s3_key"] + "/tokens.json",
-            Body=json.dumps(tokens, indent=4),
-        )
+        Returns:
+            A dict with the tokens.
+        """
+        with open(self.config["blob_storage_path"], "r", transport_params={"client": self.azure_client}) as token_file:
+            tokens = json.load(token_file)
+
+        return tokens
+    
+    def update_tokens_in_blob(self, tokens: dict) -> None:
+        """Updates Exact Online tokens in Azure blob storage using smart_open.
+
+        Args:
+            tokens: A dict with the tokens.
+        """
+        with open(self.config["blob_storage_path"], "w", transport_params={"client": self.azure_client}) as token_file:
+            json.dump(tokens, token_file)
 
     @property
     def oauth_request_body(self) -> dict:
-        """Define the OAuth request body for the AutomaticTestTap API.
+        """Define the OAuth request body for the Exact Online API.
 
         Returns:
             A dict with the request body
         """
         return {
             "refresh_token": self.refresh_token,
-            "grant_type": "refresh_token",
             "client_id": self.config["client_id"],
             "client_secret": self.config["client_secret"],
+            "grant_type": "refresh_token",
         }
 
     @backoff.on_exception(backoff.expo, EmptyResponseError, max_tries=5, factor=2)
     def update_access_token(self) -> None:
+        """Update the access token."""
+
+        # Get the current time to calculate the token expiration
         request_time = utc_now()
+
+        # Set the OAuth request payload
         auth_request_payload = self.oauth_request_payload
+
+        # Make the OAuth request to the authentication endpoint
         token_response = requests.post(
             self.auth_endpoint,
             headers=self._oauth_headers,
@@ -75,12 +101,7 @@ class ExactAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
             timeout=60,
         )
 
-        try:
-            if token_response.json().get("error_description") == "Rate limit exceeded: access_token not expired":
-                return None
-        except Exception as e:
-            raise EmptyResponseError(f"Failed converting response to a json, because response is empty")
-
+        # Raise an error if the request was not successful
         try:
             token_response.raise_for_status()
         except requests.HTTPError as ex:
@@ -90,17 +111,28 @@ class ExactAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
         self.logger.info("OAuth authorization attempt was successful.")
 
         token_json = token_response.json()
+
         self.access_token = token_json["access_token"]
         self.refresh_token = token_json["refresh_token"]
+
         expiration = token_json.get("expires_in", self._default_expiration)
+
         self.expires_in = int(expiration) if expiration else None
+
         if self.expires_in is None:
             self.logger.debug(
                 "No expires_in receied in OAuth response and no "
                 "default_expiration set. Token will be treated as if it never "
                 "expires.",
             )
+
         self.last_refreshed = request_time
 
-        tokens = {"access_token": self.access_token, "refresh_token": self.refresh_token}
-        self.put_tokens_in_s3(tokens)
+        # Update the tokens in Azure blob storage
+        tokens = {
+            "access_token": self.access_token, 
+            "refresh_token": self.refresh_token,
+            "last_refreshed": self.last_refreshed.to_iso8601_string()
+        }
+        
+        self.update_tokens_in_blob(tokens)
